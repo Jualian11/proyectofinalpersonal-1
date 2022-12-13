b9aff27c62a36e356e3915abcff0fd566b2a9420
405 <405@academicos.cesde.edu.co>
2022-12-13 11:19:53 -0500

    esa vuelta

--- src/AmpliarInfo/AmpliarInfo.js
+++ src/AmpliarInfo/AmpliarInfo.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import './AmpliarInfo.css'
 import { useLocation } from "react-router-dom"
 
 export function AmpliarInfo(){
@@ -12,21 +12,39 @@ export function AmpliarInfo(){
 
          
         <>
-            <h1 className="my-3 text-center">{producto.nombre}</h1>
-           
-            <div className="col h-100">
-                <div className="clearfix">
-                    <img src={producto.foto} className="col-md-6 float-md-end mb-3 ms-md-3 img-fluid rounded-1" alt="fotico"/>
+            <div className="container">
 
-                    <h5>dddddddddddddddddddddddddddddd
-                        dddddddddddddddddddddddddddddd
-                        dddddddddddddddddddddddddddddd
-                        dddddddddddddddddddddddddddddd
-                        dddddddddddddddddddddddddddddd</h5>
-                    <br/>
-    
+                <div className="row my-5">
 
-  
+                    <div className="col-12 col-md-5 border-end">
+                        <img src={producto.foto} alt="fotitco" className="img-fluid w-100 "/>
+                    </div>
+                    <div className="col-12 col-md-6 border p-4">
+                        <h2 className="fw-bold">{producto.nombre}</h2>
+                    <div className="precio"> 
+                        <h4>Precio: {producto.precio} -<span className="descuento "> 25% Descuento</span></h4>
+                        <h5 className="badge rounded-pill text-bg-success text-white my-4">Decuento del dia </h5>
+                        <p> Hasta 48 Cuotas</p>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/symbols.png?alt=media&token=6fb8c76e-fd84-46c0-b217-f3a0cb90e7ea" alt="visa" className="img-fluid" />
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/american-express.png?alt=media&token=15eac05a-2b00-44d2-a7a2-d26102665641" alt="american-express" className="ms-4 img-fluid" />
+
+
+                        <hr/>
+                        <i class="bi bi-truck mt-5 ms-1 fs-1 text-success "><span className='camion text-success ms-2'>  - !Envio gratis a nivel Nacional¡</span></i>
+                        <p className='camion'> Conoce los tiempos y las formas de envio.</p>
+
+                        <i class="bi bi-arrow-clockwise mt-5 ms-3 fs-1 "><span className='camion'> !Devolución gratis¡</span></i>
+                        <p className='camion ms-3'> 30 Dias para rembolso.</p>
+
+                        <hr/>
+                        <h3 className='me-3'>Descripcion: <span>{producto.descripcion}</span></h3>
+
+
+                    </div>
+
+                    </div>
+                    <div></div>
+
                 </div>
 
             </div>
--- src/Mercancia/Mercancia.js
+++ src/Mercancia/Mercancia.js
@@ -21,32 +21,43 @@ export function Mercancia(){
         {
             nombre:"T-shirt Original Importada",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/incubus%20t-shirt-1.jpg?alt=media&token=04641460-5014-4f1b-a13b-755c0cf3ce30",
+            descripcion:"100% en Algodon, eco-friendly, todas las tallas, todos los generos, animal-friendly, lgtbq+++, sin colorantes artificiales, original importada, de la tienda a tu casa. ;)",
             precio:40 +"$"
+            
         },
         {
             nombre:"T-shirt Original Importada",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/incubus%20t-shirt.jpg?alt=media&token=ad5b0f75-1f5e-4802-b1e0-9692ab97fbce",
+            descripcion:"100% en Algodon, eco-friendly, todas las tallas, todos los generos, animal-friendly, lgtbq+++, sin colorantes artificiales, original importada, de la tienda a tu casa. ;)",
             precio:40 +"$"
+            
         },
         {
             nombre:"Tank Original Importada",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/incubus%20tank.jpg?alt=media&token=0d2d1b11-c8e4-4951-b8f1-67c1f6ce3997",
+            descripcion:"100% en Algodon, eco-friendly, todas las tallas, todos los generos, animal-friendly, lgtbq+++, sin colorantes artificiales, original importada, de la tienda a tu casa. ;)",
             precio:30 +"$"
+            
         },
         {
             nombre:"Sweater Original Importada",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/incubus%20sweater-1.jpeg?alt=media&token=e6d13ac7-73f3-4544-8587-bc488d440abc",
+            descripcion:"100% en Algodon, eco-friendly, todas las tallas, todos los generos, animal-friendly, lgtbq+++, sin colorantes artificiales, original importada, de la tienda a tu casa. ;)",
             precio:60 +"$"
+            
         },
         {
             nombre:"Sweater Original Importada",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/incubus%20sweater-2.jpeg?alt=media&token=bf07edea-f10d-482b-9397-fda312275f93",
+            descripcion:"100% en Algodon, eco-friendly, todas las tallas, todos los generos, animal-friendly, lgtbq+++, sin colorantes artificiales, original importada, de la tienda a tu casa. ;)",
             precio:60 +"$"
+            
         },
         {
             nombre:"Sweater Original Importada",
             foto:"https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/incubus%20sweater.jpeg?alt=media&token=f2af47d9-0ca0-4245-825f-6aa7c87977e6",
-            precio:60 +"$"
+            precio:60 +"$",
+            descripcion:"100% en Algodon, eco-friendly, todas las tallas, todos los generos, animal-friendly, lgtbq+++, sin colorantes artificiales, original importada, de la tienda a tu casa. ;)"
         }
         
     ]
